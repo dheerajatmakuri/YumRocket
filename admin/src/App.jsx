7447b636467d4b757054096e57f402cd5e10455f
Dheeraj atmakuri <dheerajatmakuri10010@gmail.com>
2024-07-28 01:00:17 +0530

    modified:   admin/src/App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 const App = () => {
 
   // const url = "http://localhost:4000"
-  export const url = 'https://yumrocket-backend.onrender.com'
+  const url = 'https://yumrocket-backend.onrender.com'
 
   
   return (
